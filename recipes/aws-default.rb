99a4e198dec3dc7e9b68149c4eee02f132a24716
Bob Torgerson <rltorgerson@alaska.edu>
2021-05-20 11:25:40 -0800

    Removed DNS resolver changes for AWS to prevent incorrect DNS entries.

--- recipes/aws-default.rb
+++ recipes/aws-default.rb
@@ -17,12 +17,6 @@ package %w(bind-utils unzip deltarpm yum-utils ethtool git lsof net-tools pciuti
   action :install
 end
 
-resolver_config '/etc/resolv.conf' do
-  nameservers ['137.229.15.5','137.229.5.191']
-  domain 'snap.uaf.edu'
-  search ['snap.uaf.edu','rcs.alaska.edu','alaska.edu']
-end
-
 # Sets up 30 minute default Chef client runs
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
