d3a3383da2fe3074e6068f4f55d66185bce9026f
Bob Torgerson <rltorgerson@alaska.edu>
2024-01-09 11:02:10 -0500

    Removes unused portions of AWS recipe.

--- recipes/aws-default.rb
+++ recipes/aws-default.rb
@@ -8,16 +8,16 @@
 # timezone 'America/Anchorage'
 
 # Basic package installs
-package %w(bind-utils unzip deltarpm yum-utils ethtool git lsof net-tools pciutils psmisc screen strace tcpdump vim wget) do
-  action :install
-end
+# package %w(bind-utils unzip deltarpm yum-utils ethtool git lsof net-tools pciutils psmisc screen strace tcpdump vim wget) do
+#   action :install
+# end
 
 # Sets up 30 minute default Chef client runs
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
 
 # Sets up NTPd to keep clock in sync
-include_recipe 'ntp::default'
+# include_recipe 'ntp::default'
 
 include_recipe 'snap-base::_admins'
 include_recipe 'snap-base::_snap_users'
