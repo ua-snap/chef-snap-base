1cf407fe5bcbdadaae0e065cd8326a1595f5f2e3
Bob Torgerson <rltorgerson@alaska.edu>
2024-02-21 16:38:06 -0900

    Adds the installation of Miniconda3 and Prefect to the base environment if not installed already.

---
+++ recipes/_miniconda_installer.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook:: snap-base
+# Recipe:: _miniconda_installer
+#
+# Copyright:: 2024, The Authors, All Rights Reserved.
+
+# Define the URL for Miniconda3 installer
+miniconda_url = 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+
+# Define the installation directory
+miniconda_install_dir = '/opt/miniconda3'
+
+# Download the Miniconda installer
+remote_file '/tmp/miniconda.sh' do
+  source miniconda_url
+  mode '0755'
+  action :create
+end
+
+# Execute the Miniconda installer
+execute 'install_miniconda' do
+  command "/bin/bash /tmp/miniconda.sh -b -p #{miniconda_install_dir}"
+  not_if { ::File.exist?("#{miniconda_install_dir}/bin/conda") }
+  action :run
+end
+
+execute 'install_prefect' do
+    command "#{miniconda_install_dir}/bin/pip install -U prefect"
+    action :run
+end
