287d0a93ea012a22e205aaa16644450bcbd5d733
Bob Torgerson <rltorgerson@alaska.edu>
2024-08-26 08:51:57 -0800

    Opens port 7000-7020 to world for federation.

--- recipes/_rasdaman_enterprise_firewall.rb
+++ recipes/_rasdaman_enterprise_firewall.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2021, UAF SNAP, All Rights Reserved.
 
-firewall_rule "Open port 7000 to the World" do
-  port 7000
+firewall_rule "Open port 7000-7020 to the World" do
+  port 7000..7020
   command :allow
 end
