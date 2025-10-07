97efce8874e029b8e0ff8f3e935850669a58e6c0
Bob Torgerson <rltorgerson@alaska.edu>
2025-10-07 10:27:06 -0800

    Opens Prometheus port to snapweb.rcs.alaska.edu.

--- recipes/_prometheus_web_firewall.rb
+++ recipes/_prometheus_web_firewall.rb
@@ -9,3 +9,9 @@ firewall_rule "Prometheus port open to Apollo" do
     source '137.229.113.241'
     command :allow
 end
+
+firewall_rule "Prometheus port open to snapweb.rcs.alaska.edu" do
+    port 9090
+    source '199.165.82.232'
+    command :allow
+end
